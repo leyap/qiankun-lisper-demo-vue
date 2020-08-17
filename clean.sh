5edfb4f9a7f34bd58bd38d6fc5b1d182f90c45ee
lisper <liyaping@xd.com>
2020-08-17 13:43:06 +0800

    update, remove sass

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf  root-demo/node_modules
+rm -rf  child1-demo/node_modules
+rm -rf  child2-demo/node_modules
--- root-demo/src/App.vue
+++ root-demo/src/App.vue
@@ -25,7 +25,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style >
   html, body {
     padding: 0;
     margin: 0;
